2b7c692f08aa314b4c7edc0122a3f39b839c4aff
Mohieddine Zarif <mohieddine.zarif@gotocme.com>
2020-04-15 11:32:11 +0300

    Updating files

--- Pods/SwrveSDK/SwrveSDK/SDK/Messaging/UISwrveButton.m
+++
@@ -1,8 +0,0 @@
-#import "UISwrveButton.h"
-
-@implementation UISwrveButton
-
-@synthesize displayString;
-@synthesize actionString;
-
-@end
--- Pods/Target Support Files/ZappAnalyticsPluginSwrvePlugin/ZappAnalyticsPluginSwrvePlugin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZappAnalyticsPluginSwrvePlugin : NSObject
-@end
-@implementation PodsDummy_ZappAnalyticsPluginSwrvePlugin
-@end
