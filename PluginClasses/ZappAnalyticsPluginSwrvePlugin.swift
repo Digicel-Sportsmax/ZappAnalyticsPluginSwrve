1df6299215c6eb26dfc91b6f8c4224c41992902c
Mohieddine Zarif <mohieddine.zarif@gotocme.com>
2020-04-17 13:08:25 +0300

    Updating

--- PluginClasses/ZappAnalyticsPluginSwrvePlugin.swift
+++ PluginClasses/ZappAnalyticsPluginSwrvePlugin.swift
@@ -61,18 +61,6 @@ public class ZappAnalyticsPluginSwrvePlugin: ZPAnalyticsProvider {
             #endif
             ZappAnalyticsPluginSwrvePlugin.isAutoIntegrated = true
         }
-    
-        if #available(iOS 10.0, *) {
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-        } else {
-            let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound],
-                                                                                  categories: nil)
-            UIApplication.shared.registerUserNotificationSettings(settings)
-        }
-        UIApplication.shared.registerForRemoteNotifications()
 
     }
     
