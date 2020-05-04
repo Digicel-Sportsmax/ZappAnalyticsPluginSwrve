e95b6462311485636a5d130e2b5a847f2c520994
Mohieddine Zarif <mohieddine.zarif@gotocme.com>
2020-05-05 01:24:10 +0300

    added Rich Push notification class

---
+++ PluginClasses/NotificationService.swift
@@ -0,0 +1,33 @@
+//
+//  ZappAnalyticsPluginSwrvePlugin
+//  ZappAnalyticsPluginSwrve
+//
+//  Created by Mohieddine Zarif on 4/30/20.
+//  Copyright © 2020 CME. All rights reserved.
+//
+
+import UserNotifications
+import SwrveSDKCommon
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+    let groupId = "com.sportsmax.SwrveRichPushExtension"
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+
+        SwrvePush.handle(request.content, withAppGroupIdentifier: groupId) {(content) in
+            self.bestAttemptContent = content
+            contentHandler(self.bestAttemptContent!)
+        }
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
